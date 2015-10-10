0b570de89ff6c9ca0e27bb3d0913899f2ef5cdef
wangdiyuan <wangdiyuan@smartisan.com>
2015-10-10 16:53:53 +0800

    add statusbarview
    Signed-off-by: wangdiyuan <wangdiyuan@smartisan.com>

---
+++ app/src/main/java/org/zarroboogs/maps/widget/StatusBarView.java
@@ -0,0 +1,49 @@
+package org.zarroboogs.maps.widget;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+/**
+ * Created by wangdiyuan on 15-10-10.
+ */
+public class StatusBarView extends View {
+
+    public StatusBarView(Context context) {
+        super(context);
+    }
+
+
+    public StatusBarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public StatusBarView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public StatusBarView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(widthMeasureSpec, getStatusBarHeight());
+    }
+
+    public int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+}
